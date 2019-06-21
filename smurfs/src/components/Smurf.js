638bd80f75ad58fa252a7ce13153253f8dfbcb4f
Nabeelah <nabeelahyousuph@gmail.com>
2019-06-21 10:18:35 +0100

    Implement delete functionality in smurf component

--- smurfs/src/components/Smurf.js
+++ smurfs/src/components/Smurf.js
@@ -1,11 +1,20 @@
 import React from "react";
+import { connect } from "react-redux";
+import { deleteSmurfs } from "../actions";
 
 const Smurf = props => {
+  const deleteSmurf = id => {
+    props.deleteSmurfs(id);
+  };
   return (
     <div>
       <h3>{props.smurf.name}</h3>
+      <span onClick={() => deleteSmurf(props.smurf.id)}>x</span>
     </div>
   );
 };
 
-export default Smurf;
+export default connect(
+  null,
+  { deleteSmurfs }
+)(Smurf);
