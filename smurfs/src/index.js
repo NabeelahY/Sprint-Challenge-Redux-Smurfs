cc855e5affe1904e4f9a3b454362f7a762d6cc7e
Nabeelah <nabeelahyousuph@gmail.com>
2019-06-21 08:40:23 +0100

    Pass the reducer to the store and create smurf component

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -6,10 +6,10 @@ import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
-import /* You need some sort of reducer */ './reducers';
+import { smurfReducer } from './reducers';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(composeEnhancers(applyMiddleware(thunk, logger))
+const store = createStore(smurfReducer, composeEnhancers(applyMiddleware(thunk, logger))
  // this is the most basic reducer. A function that returns and object. Replace it.
   /* be sure to throw in the proper middlewares here*/
 );
