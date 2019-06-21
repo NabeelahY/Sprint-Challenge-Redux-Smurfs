03511cbd78facc98d4087b99acec32f1dabeaec3
Nabeelah <nabeelahyousuph@gmail.com>
2019-06-21 09:39:14 +0100

    Connect add smurf action dispatcher to smurf form component
    - add onSubmit function that calls smurf action dispatcher

--- smurfs/src/components/SmurfForm.js
+++ smurfs/src/components/SmurfForm.js
@@ -1,14 +1,33 @@
 import React from "react";
+import { connect } from "react-redux";
+import { addSmurf } from "../actions";
 
-const SmurfForm = () => {
+const SmurfForm = props => {
+  console.log(props);
+  const nameRef = React.createRef();
+  const ageRef = React.createRef();
+  const heightRef = React.createRef();
+
+  const addNewSmurf = e => {
+    e.preventDefault();
+    const name = nameRef.current.value;
+    const age = ageRef.current.value;
+    const height = heightRef.current.value;
+
+    props.addSmurf(name, age, height).then(() => props.history.push("/"));
+  };
+
   return (
-    <form>
-      <input type="text" placeholder="Enter name" />
-      <input type="number" placeholder="Enter age" />
-      <input type="text" placeholder="Enter height" />
+    <form onSubmit={addNewSmurf}>
+      <input type="text" placeholder="Enter name" ref={nameRef} />
+      <input type="number" placeholder="Enter age" ref={ageRef} />
+      <input type="text" placeholder="Enter height" ref={heightRef} />
       <button type="submit">Add Sumrf</button>
     </form>
   );
 };
 
-export default SmurfForm;
+export default connect(
+  null,
+  { addSmurf }
+)(SmurfForm);
