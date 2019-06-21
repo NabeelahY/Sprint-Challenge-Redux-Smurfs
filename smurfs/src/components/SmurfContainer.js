e2b2896387e19f1fdd684362c270124ad234d543
Nabeelah <nabeelahyousuph@gmail.com>
2019-06-21 11:27:17 +0100

    Style smurf container

--- smurfs/src/components/SmurfContainer.js
+++ smurfs/src/components/SmurfContainer.js
@@ -1,14 +1,21 @@
-import React from 'react'
-import Smurf from './Smurf';
+import React from "react";
+import styled from "styled-components";
+import Smurf from "./Smurf";
 
 const SmurfContainer = props => {
-    return (
-        <div>
-           {props.smurfs.map(smurf => {
-               return <Smurf key={smurf.id} smurf={smurf}/>
-           })} 
-        </div>
-    )
-}
+  return (
+    <StyledContainer>
+      {props.smurfs.map(smurf => {
+        return <Smurf key={smurf.id} smurf={smurf} />;
+      })}
+    </StyledContainer>
+  );
+};
 
-export default SmurfContainer
+export default SmurfContainer;
+
+const StyledContainer = styled.div`
+  margin: 0 auto;
+  width: 800px;
+  text-align: center;
+`;
