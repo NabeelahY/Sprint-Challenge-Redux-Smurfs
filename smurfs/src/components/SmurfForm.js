71b671fb0822bf7e7fbff468e912e9e5eec38a1b
Nabeelah <nabeelahyousuph@gmail.com>
2019-06-21 11:19:56 +0100

    Style form with style components

--- smurfs/src/components/SmurfForm.js
+++ smurfs/src/components/SmurfForm.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
+import styled from "styled-components";
 import { addSmurf } from "../actions";
 
 const SmurfForm = props => {
@@ -18,12 +19,12 @@ const SmurfForm = props => {
   };
 
   return (
-    <form onSubmit={addNewSmurf}>
+    <StyledForm onSubmit={addNewSmurf}>
       <input type="text" placeholder="Enter name" ref={nameRef} />
       <input type="number" placeholder="Enter age" ref={ageRef} />
       <input type="text" placeholder="Enter height" ref={heightRef} />
-      <button type="submit">Add Sumrf</button>
-    </form>
+      <button type="submit">Add Smurf</button>
+    </StyledForm>
   );
 };
 
@@ -31,3 +32,36 @@ export default connect(
   null,
   { addSmurf }
 )(SmurfForm);
+
+const StyledForm = styled.form`
+  margin: 0 auto;
+  padding: 30px;
+  width: 400px;
+  height: auto;
+  border-radius: 10px;
+  * {
+      box-sizing: border-box;
+  }
+
+  input {
+    margin: 15px 0;
+    padding: 15px 10px;
+    width: 100%;
+    outline: none;
+    font-size: 1rem;
+    border: 1px solid #bbb;
+    border-radius: 20px;
+  }
+
+  button {
+    display:block;
+    text-align: center;
+    font-size: 1rem;
+    margin: auto 0;
+    width: 100%;
+    padding: 15px;
+    border: none;
+    border-radius: 20px;
+    background-color: #96CDFF;
+  }
+`;
