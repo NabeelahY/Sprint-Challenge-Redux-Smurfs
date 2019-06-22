4916e3e244fb8ff1ca9f31f683ddbee171aacf23
Nabeelah <nabeelahyousuph@gmail.com>
2019-06-22 17:32:48 +0100

    Implement loader when get smurf action creator is loading smurfs

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 // import React, { useEffect } from 'react';
 import { connect } from "react-redux";
+import Loader from "react-loader-spinner";
+import styled from "styled-components";
 import { getSmurfs } from "../actions";
 import SmurfContainer from "../components/SmurfContainer";
 import SmurfForm from "./SmurfForm";
@@ -14,8 +16,19 @@ class App extends Component {
   render() {
     return (
       <div>
-        <SmurfForm />
-        {this.props.smurfs && <SmurfContainer smurfs={this.props.smurfs} />}
+        {this.props.fetchingSmurfs && (
+          <StyledLoader>
+            <Loader type="Rings" color="#96CDFF" height={80} width={80} />
+            {/* <Loader type="TailSpin" color="#96CDFF" height={80} width={80} /> */}
+          </StyledLoader>
+        )}
+
+        {this.props.smurfs && (
+          <>
+            <SmurfForm />
+            <SmurfContainer smurfs={this.props.smurfs} />
+          </>
+        )}
       </div>
     );
   }
@@ -29,5 +42,11 @@ const mapStateToProps = state => ({
 
 export default connect(
   mapStateToProps,
-  {getSmurfs}
+  { getSmurfs }
 )(App);
+
+const StyledLoader = styled.div`
+  position: absolute;
+  left: 50%;
+  top: 50%;
+`;
